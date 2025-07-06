b4745e0b7b6af89cfe2e3411e0609183a7d93b9f
leeyoung000601 <yyt601@tukorea.ac.kr>
2025-07-06 21:48:16 +0900

    0706

---
+++ app/src/main/java/com/youngtaek/mysolelife/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.youngtaek.mysolelife
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.youngtaek.mysolelife.auth.IntroActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        },3000)
+    }
+}
